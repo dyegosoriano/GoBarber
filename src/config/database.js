861c7135099acc957bcbf202307aa82a5ad62e7f
Dyego Soriano <dyegodf2@gmail.com>
2020-02-11 12:21:54 -0300

    initial sequelize setup

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  pasword: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
