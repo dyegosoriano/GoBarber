1d445b8fd256c95923a6ad2f6d166fe6ff73311c
Dyego Soriano <dyegodf2@gmail.com>
2020-02-12 15:37:29 -0300

    creating user registry

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express'
-import User from './app/models/Users'
+
+import UserController from './app/controllers/UserController'
 
 const routes = new Router()
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Dyego Soriano',
-    email: 'dyegodf2@gmail.com',
-    password_hash: '123456'
-  })
-
-  return res.json(user)
-})
+routes.post('/users', UserController.store)
 
 export default routes
