e493ca5cd0556df0ee79d8c2d0d332513b8246cc
Dyego Soriano <dyegodf2@gmail.com>
2020-04-08 14:37:10 -0300

    Validação de datas

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,9 +1,12 @@
+import * as Yup from 'yup'
+import { startOfHour, parseISO, isBefore } from 'date-fns'
+
 import Appointment from '../models/Appointments'
 import User from '../models/User'
-import * as Yup from 'yup'
 
 class AppointmentController {
   async store (request, response) {
+    // Validando campos de entrada com Yup
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
       date: Yup.date().required()
@@ -22,12 +25,30 @@ class AppointmentController {
     if (!isProvider) {
       return response.status(401).json({ error: 'You can only appointments with providers' })
     }
-    console.log('Cheguei')
+
+    // Verificando se a data de agendamento não é antiga
+    const hourStart = startOfHour(parseISO(date))
+    if (isBefore(hourStart, new Date())) {
+      return response.status(400).json({ error: 'Past dates are not permitted' })
+    }
+
+    // Verificando se existe hoário disponível
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart
+      }
+    })
+
+    if (checkAvailability) {
+      return response.status(401).json({ error: 'Appointment date is not available' })
+    }
 
     const appointment = await Appointment.create({
       user_id: request.userId,
       provider_id,
-      date
+      date: hourStart
     })
 
     return response.json(appointment)
