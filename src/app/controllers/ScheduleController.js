0a74a7c2eccaefb51d4e9a96bc33d8c1b985c6ca
Dyego Soriano <dyegodf2@gmail.com>
2020-04-09 13:18:45 -0300

    Controller de exibição de serviços

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,42 @@
+// Controller reponsável pela exibição dos serviços agendados
+
+import { startOfDay, endOfDay, parseISO } from 'date-fns'
+import { Op } from 'sequelize'
+
+import Appointment from '../models/Appointments'
+import User from '../models/User'
+
+class ScheduleController {
+  async index (req, res) {
+    const checkUserProvider = await User.findOne({
+      where: {
+        id: req.userId,
+        provider: true
+      }
+    })
+
+    if (!checkUserProvider) {
+      return res.status(401).json({ error: 'User is not a provider' })
+    }
+
+    // Formatando data
+    const { date } = req.query
+    const parsedDate = parseISO(date)
+
+    // Buscando serviços agendados com filtro de data
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)]
+        }
+      },
+      order: ['date']
+    })
+
+    return res.json(appointments)
+  }
+}
+
+export default new ScheduleController()
