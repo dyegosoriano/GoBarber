f243ffc9efd2d795c072c15e75d05e23cf165104
Dyego Soriano <dyegodf2@gmail.com>
2020-04-16 18:19:07 -0300

    Tratamento de erros com sentry

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 import express from 'express'
 import { resolve } from 'path'
+import Youch from 'youch'
+import * as Sentry from '@sentry/node'
+import 'express-async-errors'
+
 import routes from './routes'
+import sentryConfig from './config/sentry'
 
 import './database'
 
@@ -8,11 +13,15 @@ class App {
   constructor () {
     this.server = express()
 
+    Sentry.init(sentryConfig)
+
     this.middlewares()
     this.routes()
+    this.exceptionHandler()
   }
 
   middlewares () {
+    this.server.use(Sentry.Handlers.requestHandler())
     this.server.use(express.json())
     // Usando método Static para habilitar a visualização de arquivos do servidor
     this.server.use('/files', express.static(resolve(__dirname, '..', 'tmp', 'uploads')))
@@ -20,6 +29,15 @@ class App {
 
   routes () {
     this.server.use(routes)
+    this.server.use(Sentry.Handlers.errorHandler())
+  }
+
+  exceptionHandler () {
+    this.server.use(async (err, request, response, next) => {
+      const errors = await new Youch(err, request).toJSON()
+
+      return response.status(500).json(errors)
+    })
   }
 }
 
