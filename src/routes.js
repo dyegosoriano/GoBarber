32ce5f7a086cdb9daf0f14e3e51c56eb88f60c91
Dyego Soriano <dyegodf2@gmail.com>
2020-04-16 16:57:40 -0300

    Listanto horários disponiveis

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import ProviderController from './app/controllers/ProviderController'
 import AppointmentController from './app/controllers/AppointmentController'
 import ScheduleController from './app/controllers/ScheduleController'
 import NotificationController from './app/controllers/NotificationController'
+import AvailableController from './app/controllers/AvailableController'
 
 import authMiddleware from './app/middlewares/auth'
 
@@ -23,12 +24,17 @@ routes.use(authMiddleware)
 routes.put('/users', UserController.update)
 
 routes.get('/providers', ProviderController.index)
+routes.get('/providers/:providerId/available', AvailableController.index)
+
 routes.get('/appointments', AppointmentController.index)
 routes.post('/appointments', AppointmentController.store)
 routes.delete('/appointments/:id', AppointmentController.delete)
+
 routes.get('/notifications', NotificationController.index)
 routes.put('/notifications/:id', NotificationController.update)
+
 routes.get('/schedule', ScheduleController.index)
+
 routes.post('/files', upoad.single('file'), FileController.store)
 
 export default routes
