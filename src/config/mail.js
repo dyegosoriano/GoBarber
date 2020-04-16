ac0f321f49215f5376f5dfcacfe5a7a7b5468e9d
Dyego Soriano <dyegodf2@gmail.com>
2020-04-16 14:18:00 -0300

    Configurando templates de e-mail

--- src/config/mail.js
+++ src/config/mail.js
@@ -3,8 +3,8 @@ export default {
   port: 2525,
   secure: false,
   auth: {
-    user: '',
-    pass: ''
+    user: 'cadefd00811329',
+    pass: '5e30f5530cf761'
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>'
